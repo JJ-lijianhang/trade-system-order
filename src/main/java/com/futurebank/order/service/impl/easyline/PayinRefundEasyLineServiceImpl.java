2d55d28eb5edee5e05512f3b1d143b68aa72bcf0
ljh <lijianhang@futurebank.global>
2025-03-26 11:31:56 +0800

    修复包导入

--- src/main/java/com/futurebank/order/service/impl/easyline/PayinRefundEasyLineServiceImpl.java
+++ src/main/java/com/futurebank/order/service/impl/easyline/PayinRefundEasyLineServiceImpl.java
@@ -27,7 +27,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
+import jakarta.annotation.Resource;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
